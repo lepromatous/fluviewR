fd04f34615ae13bc34d0facf1daf7a2ebfb46c16
Wiemken <WIEMKT@pfizer.com>
2021-09-30 16:30:10 -0500

    cleaned

--- fluview_functions.R
+++ fluview_functions.R
@@ -8,7 +8,7 @@ library(tidyverse)
 ### Loc = "clinical", "public health", "ILI", or "ILI age [character]
 ### start = number for start year of influenza season of interest
 ### stop = number for stop year of influenza season of interest
-fluview.scrape <- function(loc, start, stop) {
+fluview.scrape <- function(loc, start, stop = start+1) {
   if(loc == "clinical"){
     url <- paste0("https://www.cdc.gov/flu/weekly/weeklyarchives", start, "-", stop, "/data/whoAllregt_cl37.html")
   } else if(loc == "public health"){
@@ -31,22 +31,22 @@ fluview.scrape <- function(loc, start, stop) {
 ### EXAMPLE RUN SCRIPTS FOR BOTH CLINICAL AND PUBLIC HEALTH 2015 - 2021
 df.clin <-
   rbind(
-    fluview.scrape(loc ="clinical", start=2015, stop=2016),
-    fluview.scrape(loc ="clinical", start=2016, stop=2017),
-    fluview.scrape(loc ="clinical", start=2017, stop=2018),
-    fluview.scrape(loc ="clinical", start=2018, stop=2019),
-    fluview.scrape(loc ="clinical", start=2019, stop=2020),
-    fluview.scrape(loc ="clinical", start=2020, stop=2021)
+    fluview.scrape(loc ="clinical", start=2015),
+    fluview.scrape(loc ="clinical", start=2016),
+    fluview.scrape(loc ="clinical", start=2017),
+    fluview.scrape(loc ="clinical", start=2018),
+    fluview.scrape(loc ="clinical", start=2019),
+    fluview.scrape(loc ="clinical", start=2020)
   )
 
 df.ph <-
   rbind(
-    fluview.scrape(loc ="public health", start=2015, stop=2016),
-    fluview.scrape(loc ="public health", start=2016, stop=2017),
-    fluview.scrape(loc ="public health", start=2017, stop=2018),
-    fluview.scrape(loc ="public health", start=2018, stop=2019),
-    fluview.scrape(loc ="public health", start=2019, stop=2020),
-    fluview.scrape(loc ="public health", start=2020, stop=2021)
+    fluview.scrape(loc ="public health", start=2015),
+    fluview.scrape(loc ="public health", start=2016),
+    fluview.scrape(loc ="public health", start=2017),
+    fluview.scrape(loc ="public health", start=2018),
+    fluview.scrape(loc ="public health", start=2019),
+    fluview.scrape(loc ="public health", start=20201)
   )
 
 
